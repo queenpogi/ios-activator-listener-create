7b7570816c8cd5e0d0212154d72b0925931635ef
z448 <zed448@icloud.com>
2016-12-09 22:09:51 +0100

    README + Getopts

--- README.md
+++ README.md
@@ -6,20 +6,31 @@ create-activator-listener - CLI tool and perl module to create Activator 'Run Co
 
 If you are writting CLI tool for iOS, Activator has very usefull feature to create 'Run command' listener that can be build in GUI ( choose-gesture -> *Build* -> *Run command* and fill *Title* and *Command* ). Assigning created listener to event will execute specified command in shell. Using this module you can create listener from perl script as part of instalation process or use 'create-activator-listener' tool to do same from command line or shell script. Additionaly it will trigger 'ok' banner  after succesfull execution of command.
 
-As not everyone is fan of command line, this way user can assign Activator gesture that will trigger your tool, command, service...anyting without GUI.
+To create Activator listener during package instalation, include it in a package and put into debian 'postinst' file `create-activator-listener -c 'command -param -param'`
+As not everyone is fan of command line, this way user can install your tool and Activator gesture is created automaticaly so user can just assign it.
 
 #USAGE
 
 ```
 # from command line or shell script
-create-activator-listener 'command -param -param'
-# respring device
+
+# create listeners and respring
+create-activator-listener -c 'command -param -param' -r
+
+# create listeners without respring
+create-activator-listener -c 'command -param -param'
 ```
 
 ```perl
 # from perl script 
 use iOS::Activator::Listener::Create qw< activator_listener >;
-activator_listener("command -param -param");
+
+# if you run your script with 'setup' option, it creates listener and respring
+if( $ARGV[0] eq 'setup' ){
+    my $listener = { c => 'command -param -param2', r => 1 }
+    activator_listener( $listener );
+} 
+
 ```
 
 #INSTALLATION
--- lib/iOS/Activator/Listener/Create.pm
+++ lib/iOS/Activator/Listener/Create.pm
@@ -17,14 +17,15 @@ our @EXPORT_OK = qw( activator_listener );
 our $VERSION = '0.01';
 
 my $cache = '/var/mobile/Library/Caches/libactivator.plist';
-my $t = time();
-croak "cant find 'plutil' binary, install 'Erica Utilities' from BigBoss repository to get it" unless `which pluti`;
+my $uuid = time();
+
+croak "cant find 'plutil' binary, install 'Erica Utilities' from BigBoss repository to get it" unless `which plutil`;
 
 
 sub activatorKeys {
     my($cmd, $type) = @_;
 
-    my $name = lc $cmd; $name =~ s/([a-z].*?)(\ .*)/$1/; $name .= $t;
+    my $name = lc $cmd; $name =~ s/([a-z].*?)(\ .*)/$1/; $name .= $uuid;
     my $key = '.run.'; $key = '.show.' if $type eq 'Message';
 
     my $listener = {
@@ -40,20 +41,23 @@ sub activatorKeys {
 };
 
 sub activator_listener {
-    my $command = shift;
+    my $opt = shift;
     my $create_listener = sub {
         for(qw< Command Message >){
-            my $a =  activatorKeys("$command", $_);
+            my $a =  activatorKeys($opt->{c}, $_);
+            my $action = $a->{action}->();
             my $key = lc $_;
 
             if(`activator get $a->{type}` eq $a->{uuid}){ return }
             system("plutil -key $a->{type} -dict $cache") unless `activator get $a->{type}`;
             system("plutil -key $a->{type} -key $a->{uuid} -dict $cache");
             system("plutil -key $a->{type} -key $a->{uuid} -key title -value $a->{title} $cache");
-            system("plutil -key $a->{type} -key $a->{uuid} -key $key -value \"$a->{action}->()\" $cache");
+            system("plutil -key $a->{type} -key $a->{uuid} -key $key -value \"$action\" $cache");
             system("plutil -key $a->{type} -key $a->{uuid} -key banner -value 1 $cache") if /Message/;
         };
     };
-    if(print $create_listener->()){ system("activator send libactivator.system.respring") }
 
+    say $create_listener->();
+    if(defined $opt->{r}){ system("activator send libactivator.system.respring") }
 }
+
